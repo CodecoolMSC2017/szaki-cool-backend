f04c4b839f5ae406dc4060d5255f1e1ea9e09f4b
gaboroneszabo <puma678@gmail.com>
2018-07-17 14:07:20 +0200

    add controller for register users

---
+++ src/main/java/com/codecool/web/controller/RegisterController.java
@@ -0,0 +1,26 @@
+package com.codecool.web.controller;
+
+
+import com.codecool.web.model.User;
+import com.codecool.web.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/register")
+public class RegisterController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("")
+    public User register(@RequestBody Map<String, String> map) {
+        String username = map.get("username");
+        String password = map.get("password");
+        String email = map.get("email");
+        return userService.add(username, password, password);
+    }
+}
