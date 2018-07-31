9a2be5eeab74f5817516a3c3d0b8f5922017f74c
gaboroneszabo <puma678@gmail.com>
2018-07-31 14:03:25 +0200

    add some pics

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -29,7 +29,10 @@ INSERT INTO works (contractor, title, category, description, sharing_date, due_d
 INSERT INTO pictures (promoted, work_id, name) VALUES
     (true, 1, 'photographer.jpeg'),
     (true, 2, 'kecske.jpg'),
-    (true, 3, 'kecske.jpg')
+    (true, 3, 'kecske.jpg'),
+    (false, 1, 'paint2.jpg'),
+    (false, 1, 'paint3.jpg'),
+    (false, 1, 'paint4.jpg')
 ;
 
 INSERT INTO ratings (work_id, rating) VALUES
