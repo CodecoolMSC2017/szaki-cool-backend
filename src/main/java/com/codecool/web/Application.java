76dc91db2d573504ede4ef0b55ea3916a18086c8
gaboroneszabo <puma678@gmail.com>
2018-07-31 13:31:43 +0200

    add controller to receive files

--- src/main/java/com/codecool/web/Application.java
+++ src/main/java/com/codecool/web/Application.java
@@ -9,9 +9,6 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class Application {
 
     public static void main(String[] args) {
-
-
         SpringApplication.run(Application.class);
-
     }
 }
