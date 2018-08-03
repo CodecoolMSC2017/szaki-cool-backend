93506d6de97a19d3d7329b047ef5d393880271bf
Domokos Tibor <Domokos.Tibor.82@gmail.com>
2018-08-03 06:27:17 +0200

    nice sql datas and pics for ads

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 insert into users (username, password, email, enabled, role, activation_code) values ('admin', '$2a$10$2Gi3G9XaKalERoIa74OYruEHZyqSUqn10uSiOzk4PvOgL49vejna.','szakicool@gmail.com', true, 'ADMIN', null);
-insert into users (username, password, email, enabled, role, activation_code) values ('user', '$2a$10$gks0yrt6re61gaPnpDm6LOXgqQ3QyBr5VZlZQJzF2FnGJ6fMCN0.G','szakicool-user@gmail.com', true, 'USER', null);
+insert into users (username, password, email, enabled, role, activation_code) values ('Jimmy', '$2a$10$gks0yrt6re61gaPnpDm6LOXgqQ3QyBr5VZlZQJzF2FnGJ6fMCN0.G','szakicool-user@gmail.com', true, 'USER', null);
 insert into authorities (username, authority) values ('admin', 'ROLE_ADMIN');
 
 
@@ -23,12 +23,12 @@ INSERT INTO guarantee_length (guarantee_length) VALUES
 ;
 
 INSERT INTO works (contractor, title, category, description, sharing_date, due_date, price, currency_id, guarantee_value, guarantee_length_id, starting_date, bid, min_bidder_user_rate, bid_expire_date) VALUES
-    (1, 'title', 'art', 'Family photos within 1 day!', '2018-07-01 00:00:00', '2018-07-30 00:00:00', 35, 1, 6, 3, '2018-07-03 00:00:00', false, null, null),
-    (1, 'title', 'household','Wash1ng 5 dishes within 10 seconds! High quality with low price!', '2018-07-01 00:00:00', '2018-07-20 00:00:00', null, null, null, null, null, false, null, null),
+    (1, 'title', 'art', 'Family photos within 1 day!', '2018-07-01 00:00:00', '2018-07-30 00:00:00', 35, 3, 6, 3, '2018-07-03 00:00:00', false, null, null),
+    (1, 'title', 'household','Washing 5 dishes within 10 seconds! High quality with low price!', '2018-07-01 00:00:00', '2018-07-20 00:00:00', null, null, null, null, null, false, null, null),
     (1, 'title', 'gambling', 'Red goes here and black goes there...', '2018-07-01 00:00:00', '2018-07-30 00:00:00', null, null, null, null, null, true, 3, '2018-08-20 00:00:00'),
-    (2, 'title', 'butcher', 'Little piggies go to Heaven', '2018-07-01 00:00:00', '2018-07-30 00:00:00', 15, 4, 2, 1, '2018-07-03 00:00:00', false, null, null),
-    (2, 'Turbo charger upgrade', 'car mechanic','High valued service with the lowest price!', '2018-07-01 00:00:00', '2018-07-20 00:00:00', null, null, null, null, null, false, null, null),
-    (2, 'Clean dishes, easy life', 'dish washer', 'I will do no matter how dirty...', '2018-07-01 00:00:00', '2018-07-30 00:00:00', null, null, null, null, null, true, 3, '2018-08-20 00:00:00')
+    (2, 'Transcription 4 you', 'transcription', 'I will provide fast and flawless audio video transcription', '2018-07-01 00:00:00', '2018-07-30 00:00:00', 15, 3, 2, 1, '2018-07-03 00:00:00', false, null, null),
+    (2, 'Turbocharger upgrade', 'car mechanic','High valued service with the lowest price!', '2018-07-01 00:00:00', '2018-07-20 00:00:00', null, null, null, null, null, false, null, null),
+    (2, 'Clean dishes, easy life', 'household', 'I will do no matter how dirty...', '2018-07-01 00:00:00', '2018-07-30 00:00:00', null, null, null, null, null, true, 3, '2018-08-20 00:00:00')
 ;
 
 INSERT INTO pictures (promoted, work_id, name) VALUES
@@ -38,9 +38,9 @@ INSERT INTO pictures (promoted, work_id, name) VALUES
     (false, 1, 'paint2.jpg'),
     (false, 1, 'paint3.jpg'),
     (false, 1, 'paint4.jpg'),
-    (true, 4, 'paint4.jpg'),
-    (true, 5, 'paint3.jpg'),
-    (true, 6, 'paint2.jpg')
+    (true, 4, 'transcription.png'),
+    (true, 5, 'turbocharger.jpg'),
+    (true, 6, 'clean_dishes.jpg')
 ;
 
 INSERT INTO ratings (work_id, rating) VALUES
