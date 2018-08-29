3706de8cc07d681221f2f1f70bbceeb7444e7381
Domokos Tibor <Domokos.Tibor.82@gmail.com>
2018-08-29 14:24:52 +0200

    capital categoreis

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -28,19 +28,19 @@ INSERT INTO guarantee_length (guarantee_length) VALUES
 ;
 
 INSERT INTO works (contractor, title, category, description, sharing_date, due_date, price, currency_id, guarantee_value, guarantee_length_id, starting_date, bid, min_bidder_user_rate, bid_expire_date) VALUES
-    (1, 'title', 'art', 'Family photos within 1 day!', '2018-07-01 00:00:00', '2018-07-30 00:00:00', 35, 3, 6, 3, '2018-09-03 00:00:00', false, 2, null),
-    (1, 'title', 'household','Washing 5 dishes within 10 seconds! High quality with low price!', '2018-07-01 00:00:00', '2018-09-20 00:00:00', 15, 3, 6, 3, null, false, 1, null),
-    (1, 'title', 'gambling', 'Red goes here and black goes there...', '2018-07-01 00:00:00', '2018-09-30 00:00:00', 10, 3, 12, 3, null, true, 3, '2018-08-20 00:00:00'),
-    (2, 'Transcription 4 you', 'transcription', 'I will provide fast and flawless audio video transcription', '2018-09-01 00:00:00', '2018-07-30 00:00:00', 15, 3, 2, 1, '2018-07-03 00:00:00', false, 0, null),
-    (2, 'Turbocharger upgrade', 'car mechanic','High valued service with the lowest price!', '2018-07-01 00:00:00', '2018-09-20 00:00:00', 50, 3, 1, 1, null, false, 0, null),
+    (1, 'title', 'Art', 'Family photos within 1 day!', '2018-07-01 00:00:00', '2018-07-30 00:00:00', 35, 3, 6, 3, '2018-09-03 00:00:00', false, 2, null),
+    (1, 'title', 'Household','Washing 5 dishes within 10 seconds! High quality with low price!', '2018-07-01 00:00:00', '2018-09-20 00:00:00', 15, 3, 6, 3, null, false, 1, null),
+    (1, 'title', 'Gambling', 'Red goes here and black goes there...', '2018-07-01 00:00:00', '2018-09-30 00:00:00', 10, 3, 12, 3, null, true, 3, '2018-08-20 00:00:00'),
+    (2, 'Transcription 4 you', 'Transcription', 'I will provide fast and flawless audio video transcription', '2018-09-01 00:00:00', '2018-07-30 00:00:00', 15, 3, 2, 1, '2018-07-03 00:00:00', false, 0, null),
+    (2, 'Turbocharger upgrade', 'Car mechanic','High valued service with the lowest price!', '2018-07-01 00:00:00', '2018-09-20 00:00:00', 50, 3, 1, 1, null, false, 0, null),
 
-    (2, 'Clean dishes, easy life', 'household', 'I will do no matter how dirty...', '2018-07-01 00:00:00', '2018-10-03 00:00:00', 30, 3, 1, 1, null, true, 3, '2018-08-20 00:00:00'),
-    (3, 'Tattoo - Your skin, your story', 'art', 'I''m a professional tattoo artirts and also a graphic designer. With more than 6 years of experience and over 100 tattoos done by myself, i''m totally qualified to design originals tattoos for you. Feel free to contact me with no compromise. Hope we can work together! I will design the best tattoo art in the market, for the lowest price!', '2018-07-01 00:00:00', '2018-08-29 00:00:00', 80, 3, 1, 4, '2018-07-03 00:00:00', false, 5, null),
-    (3, 'I will find online jobs for you', 'art', 'I will find 2 jobs that fits your need with my guidance how to do them.', '2018-07-01 00:00:00', '2018-11-01 00:00:00', 15, 3, 1, 1, '2018-07-03 00:00:00', false, 4, null),
-    (3, 'I will give you a keto plan and recipes for 1 month', 'art', 'Everything in the Basic Plan + the Grocery List, wich will make your life way easier.', '2018-07-01 00:00:00', '2018-07-30 00:00:00', 5, 3, 1, 2, '2018-07-03 00:00:00', false, 10, null),
-    (2, 'I will design a professional menu for you', 'art', '3 to 6 sided menu design, aprox A4, high resolution. Ready to print.', '2018-07-01 00:00:00', '2018-07-30 00:00:00', 800, 3, 1, 3, '2018-09-03 00:00:00', true, 7, null),
+    (2, 'Clean dishes, easy life', 'Household', 'I will do no matter how dirty...', '2018-07-01 00:00:00', '2018-10-03 00:00:00', 30, 3, 1, 1, null, true, 3, '2018-08-20 00:00:00'),
+    (3, 'Tattoo - Your skin, your story', 'Art', 'I''m a professional tattoo artirts and also a graphic designer. With more than 6 years of experience and over 100 tattoos done by myself, i''m totally qualified to design originals tattoos for you. Feel free to contact me with no compromise. Hope we can work together! I will design the best tattoo art in the market, for the lowest price!', '2018-07-01 00:00:00', '2018-08-29 00:00:00', 80, 3, 1, 4, '2018-07-03 00:00:00', false, 5, null),
+    (3, 'I will find online jobs for you', 'Art', 'I will find 2 jobs that fits your need with my guidance how to do them.', '2018-07-01 00:00:00', '2018-11-01 00:00:00', 15, 3, 1, 1, '2018-07-03 00:00:00', false, 4, null),
+    (3, 'I will give you a keto plan and recipes for 1 month', 'Art', 'Everything in the Basic Plan + the Grocery List, wich will make your life way easier.', '2018-07-01 00:00:00', '2018-07-30 00:00:00', 5, 3, 1, 2, '2018-07-03 00:00:00', false, 10, null),
+    (2, 'I will design a professional menu for you', 'Art', '3 to 6 sided menu design, aprox A4, high resolution. Ready to print.', '2018-07-01 00:00:00', '2018-07-30 00:00:00', 800, 3, 1, 3, '2018-09-03 00:00:00', true, 7, null),
 
-    (4, 'Weapons for hire!', 'art', 'Hire a PS-3000 tool, because the problem solved when it''s source is 6 feet under. Problem Solver 3000 is your perfect choice.', '2018-07-01 00:00:00', '2018-09-23 18:00:00', 1500, 3, 1, 1, '2018-09-30 00:00:00', true, 3, null)
+    (4, 'Weapons for hire!', 'Art', 'Hire a PS-3000 tool, because the problem solved when it''s source is 6 feet under. Problem Solver 3000 is your perfect choice.', '2018-07-01 00:00:00', '2018-09-23 18:00:00', 1500, 3, 1, 1, '2018-09-30 00:00:00', true, 3, null)
 ;
 
 INSERT INTO pictures (promoted, work_id, name) VALUES
